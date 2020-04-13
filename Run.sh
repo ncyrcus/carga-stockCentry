ab6c978c4aee4e5db8ec4437f819263eabb3f1cd
Nicolás Orellana <nicosorellana@gmail.com>
2020-04-13 15:44:27 -0400

    readme

--- Run.sh
+++ Run.sh
@@ -3,5 +3,4 @@
 docker build -t cargastock .
 full_path=$(realpath $0)
 SCRIPTPATH=$(dirname $full_path)
-echo {$SCRIPTPATH/script-log/}
 docker run -it --rm --name microservicio -v $SCRIPTPATH/script-log/:/usr/src/app/log/ cargastock 
